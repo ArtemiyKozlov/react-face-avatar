a6a66f80d8cbaa72907d450c2fdd6a09c14d5bd7
Artemii <artemiy.kozlov@gmail.com>
2020-03-09 01:15:42 +0100

    add props for component box resize

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import FaceAvatar from './components/face-avatar';
 
@@ -7,16 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <FaceAvatar src='https://scontent.fktw1-1.fna.fbcdn.net/v/t1.0-9/39102597_1891603477550129_2174934819803234304_o.jpg?_nc_cat=106&_nc_sid=dd9801&_nc_oc=AQmSVv0P5Zxx8qj7XN3-Tpo697dSQ-Agd8tS9i2K8CvfgMSsGx8Qeb8mohBrwwMnKQo&_nc_ht=scontent.fktw1-1.fna&oh=64748b386f322dcd1e07c3bfebebd33d&oe=5E902D1D' />
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <FaceAvatar className="avatar" src='https://scontent.fktw1-1.fna.fbcdn.net/v/t1.0-9/39102597_1891603477550129_2174934819803234304_o.jpg?_nc_cat=106&_nc_sid=dd9801&_nc_oc=AQmSVv0P5Zxx8qj7XN3-Tpo697dSQ-Agd8tS9i2K8CvfgMSsGx8Qeb8mohBrwwMnKQo&_nc_ht=scontent.fktw1-1.fna&oh=64748b386f322dcd1e07c3bfebebd33d&oe=5E902D1D' />
+        <FaceAvatar src='https://scontent.fktw1-1.fna.fbcdn.net/v/t1.0-9/87843512_865006830625471_6411090162251464704_o.jpg?_nc_cat=107&_nc_sid=110474&_nc_oc=AQna4RZgNn_WMfnaOc-gbBObYNXf6fnYKKPc7A47gOoR7oeFPeajWlmCBTKMRqqnKDg&_nc_ht=scontent.fktw1-1.fna&oh=3df76cdc983e37550895aa553edff460&oe=5E94FF17' />
+        <FaceAvatar src='https://scontent.fktw1-1.fna.fbcdn.net/v/t1.0-9/11236430_825426500886927_5326598974952025982_n.jpg?_nc_cat=101&_nc_sid=85a577&_nc_oc=AQmINx0JMqr9409zjtKlvnycOBMmhMuiNLHq5-ymckn-uY_xdjzui4-I9pFkbK7Xl5s&_nc_ht=scontent.fktw1-1.fna&oh=8b623487b1620caef4983ec2f14afb9c&oe=5E90B6F1' />
+        <FaceAvatar src='https://image.cnbcfm.com/api/v1/image/105968995-1560512715227rtx6z8xd.jpg?v=1583511384?w=720&h=405' />
+        <FaceAvatar src='https://secure.gravatar.com/avatar/95d625fd173c40f06e3249493203cfa6?s=200&d=mm&r=g' />
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
--- src/components/face-avatar.tsx
+++ src/components/face-avatar.tsx
@@ -1,15 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as faceapi from 'face-api.js'
+import { TinyFaceDetectorOptions, Rect } from 'face-api.js';
 
-const FaceAvatar: React.FC<faceAvatarProps> = ({ src }) => {
+const load = faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+
+const FaceAvatar: React.FC<faceAvatarProps> = ({ src, className = '', showOrigin = false, width = 100, height = 100 }) => {
+    let imageRef = React.createRef<HTMLImageElement>();
+    let origin = React.createRef<HTMLImageElement>();
+
+    useEffect(() => {
+        async function extractFace() {
+            await load;
+
+            const srcImg = origin.current;
+            if (srcImg === null) return;
+
+            const detection = await faceapi.detectSingleFace(srcImg,
+                new TinyFaceDetectorOptions({ scoreThreshold: 0.3 }));
+
+            if (!detection)  {
+                showOrigin = true;
+                return;
+            }
+
+            var rect = adjustFaceBox(detection.box);
+            const canvases = await faceapi.extractFaces(srcImg, [rect]);
+            // const canvases = await faceapi.extractFaces(srcImg, [detection]);
+            var result = canvases[0];
+            if (imageRef.current) {
+                imageRef.current.src = result.toDataURL();
+            }
+        }
+        function adjustFaceBox(box: faceapi.Box): Rect {
+            const targetRatio = width / height;
+            const currentRatio = box.width / box.height;
+            let widRatio = 1;
+            let heightRatio = 1
+            if (currentRatio < targetRatio) {
+                widRatio = targetRatio / currentRatio;
+            } else {
+                heightRatio = currentRatio / targetRatio;
+            }
+
+            return new Rect(box.x - box.width / 2, box.y - box.width / 2, box.width * 2 * widRatio, box.height * 2 * heightRatio);
+        }
+
+        extractFace();
+    })
+
     return (
-        <div>
-            <img src={src} alt="avatar" />
-        </div>
+        <>
+            <img crossOrigin="anonymous" src={src} alt="sourceImage" hidden={!showOrigin} ref={origin} />
+            <img
+                hidden={showOrigin}
+                alt="avatar"
+                ref={imageRef}
+                className={className}
+                width={width}
+                height={height}
+                style={className ?  {} : { borderRadius: '50%' }} />
+        </>
+
     );
 };
 
 interface faceAvatarProps {
     src: string;
+    showOrigin?: boolean;
+    className?: string;
+    width?: number;
+    height?: number;
 }
 
-export default FaceAvatar;
+export default React.memo(FaceAvatar);
